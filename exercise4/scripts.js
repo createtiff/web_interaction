1fadb0a10ba6017ee0d3045cc2380e5f91dded9e
createtiff <createtiff@gmail.com>
2019-09-26 18:39:38 -0400

    Add files via upload

---
+++ exercise4/scripts.js
@@ -0,0 +1,379 @@
+var m = new Date().getMinutes();
+var hr = new Date().getHours();
+var s = new Date().getSeconds();
+var day = new Date().getDay();
+var date = new Date().getDate();
+
+console.log(m);
+console.log(hr);
+console.log(s);
+console.log(day);
+console.log(date);
+
+//
+
+function display_c(){
+var refresh=1000; // Refresh rate in milli seconds
+mytime=setTimeout('display_ct()',refresh)
+}
+
+
+
+function display_ct() {
+var hour = new Date().getHours();
+var min = new Date().getMinutes();
+
+  document.getElementById('hour').innerHTML = hour;
+  document.getElementById('minute').innerHTML = min;
+
+
+
+display_c();
+
+ }
+
+var hour = new Date().getHours();
+var min = new Date().getMinutes();
+var sec = new Date().getSeconds();
+
+
+
+    if(hour == 1 ) {
+        $('body').append('<div class="hour-1"></div>');
+    }
+
+    if(hour == 2 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div>');
+    }
+
+    if(hour == 3 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div>');
+    }
+
+    if(hour == 4 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div>');
+    }
+
+    if(hour == 5 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div>');
+    }
+
+    if(hour == 6 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div>');
+    }
+
+    if(hour == 7 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div>');
+    }
+
+    if(hour == 8 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div><div class="hour-8"></div>');
+    }
+
+    if(hour == 9 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div><div class="hour-8"></div><div class="hour-9"></div>');
+    }
+
+    if(hour == 10 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div><div class="hour-8"></div><div class="hour-9"></div><div class="hour-10"></div>');
+    }
+
+    if(hour == 11 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div><div class="hour-8"></div><div class="hour-9"></div><div class="hour-10"></div><div class="hour-11"></div>');
+    }
+
+    if(hour == 12 ) {
+        $('body').append('<div class="hour-1"></div><div class="hour-2"></div></div><div class="hour-3"></div><div class="hour-4"></div><div class="hour-5"></div><div class="hour-6"></div><div class="hour-7"></div><div class="hour-8"></div><div class="hour-9"></div><div class="hour-10"></div><div class="hour-11"></div><div class="hour-12"></div>');
+    }
+
+    if(hour == 13 ) {
+        $('body').append('<div class="hour-13"></div>');
+    }
+
+    if(hour == 14 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div>');
+    }
+
+    if(hour == 15 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div>');
+    }
+
+    if(hour == 16 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div>');
+    }
+
+    if(hour == 17 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div>');
+    }
+
+    if(hour == 18 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div>');
+    }
+
+    if(hour == 19 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div>');
+    }
+
+    if(hour == 20 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div><div class="hour-20"></div>');
+    }
+
+    if(hour == 21 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div><div class="hour-20"></div><div class="hour-21"></div>');
+    }
+
+    if(hour == 22 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div><div class="hour-20"></div><div class="hour-21"></div><div class="hour-22"></div>');
+    }
+
+    if(hour == 23 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div><div class="hour-20"></div><div class="hour-21"></div><div class="hour-22"></div><div class="hour-23"></div>');
+    }
+
+    if(hour == 24 ) {
+        $('body').append('<div class="hour-13"></div><div class="hour-14"></div><div class="hour-15"></div><div class="hour-16"></div><div class="hour-17"></div><div class="hour-18"></div><div class="hour-19"></div><div class="hour-20"></div><div class="hour-21"></div><div class="hour-22"></div><div class="hour-23"></div><div class="hour-24"></div>');
+    }
+
+    //
+
+    if(min == 0 ) {
+        $('body').append('');
+    }
+
+    if(min == 1 ) {
+        $('body').append('<div class="min-1"></div>');
+    }
+
+    if(min == 2 ) {
+        $('body').append('<div class="min-1"></div><div class="min-2"></div>');
+    }
+
+    if(min == 3 ) {
+        $('body').append('<div class="min-1"></div><div class="min-2"></div><div class="min-3"></div>');
+    }
+
+    if(min == 4 ) {
+        $('body').append('<div class="min-1"></div><div class="min-2"></div><div class="min-3"></div><div class="min-4"></div>');
+    }
+
+    if(min == 5 ) {
+        $('body').append('<div class="min-5"></div>');
+    }
+
+    if(min == 6 ) {
+        $('body').append('<div class="min-5"></div><div class="min-6"></div>');
+    }
+
+    if(min == 7 ) {
+        $('body').append('<div class="min-5"></div><div class="min-6"></div><div class="min-7"></div>');
+    }
+
+    if(min == 8 ) {
+        $('body').append('<div class="min-5"></div><div class="min-6"></div><div class="min-7"></div><div class="min-8"></div>');
+    }
+
+    if(min == 9 ) {
+        $('body').append('<div class="min-5"></div><div class="min-6"></div><div class="min-7"></div><div class="min-8"></div><div class="min-9"></div>');
+    }
+
+    if(min == 10 ) {
+        $('body').append('<div class="min-10"></div>');
+    }
+
+    if(min == 11 ) {
+        $('body').append('<div class="min-10"></div><div class="min-11"></div>');
+    }
+
+    if(min == 12 ) {
+        $('body').append('<div class="min-10"></div><div class="min-11"></div><div class="min-12"></div>');
+    }
+
+    if(min == 13 ) {
+        $('body').append('<div class="min-10"></div><div class="min-11"></div><div class="min-12"></div><div class="min-13"></div>');
+    }
+
+    if(min == 14 ) {
+        $('body').append('<div class="min-10"></div><div class="min-11"></div><div class="min-12"></div><div class="min-13"></div><div class="min-14"></div>');
+    }
+
+    if(min == 15 ) {
+        $('body').append('<div class="min-10"></div><div class="min-15"></div>');
+    }
+
+    if(min == 16 ) {
+        $('body').append('<div class="min-10"></div><div class="min-15"></div><div class="min-16"></div>');
+    }
+
+    if(min == 17 ) {
+        $('body').append('<div class="min-10"></div><div class="min-15"></div><div class="min-16"></div><div class="min-17"></div>');
+    }
+
+    if(min == 18 ) {
+        $('body').append('<div class="min-10"></div><div class="min-15"></div><div class="min-16"></div><div class="min-17"></div><div class="min-18"></div>');
+    }
+
+    if(min == 19 ) {
+        $('body').append('<div class="min-10"></div><div class="min-15"></div><div class="min-16"></div><div class="min-17"></div><div class="min-18"></div><div class="min-19"></div>');
+    }
+
+    if(min == 20 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div>');
+    }
+
+    if(min == 21 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-21"></div>');
+    }
+
+    if(min == 22 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-21"></div><div class="min-22"></div>');
+    }
+
+    if(min == 23 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-21"></div><div class="min-22"></div><div class="min-23"></div>');
+    }
+
+    if(min == 24 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-21"></div><div class="min-22"></div><div class="min-23"></div><div class="min-24"></div>');
+    }
+
+    if(min == 25 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-25"></div>');
+    }
+
+    if(min == 26 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-25"></div><div class="min-26"></div>');
+    }
+
+    if(min == 27 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-25"></div><div class="min-26"></div><div class="min-27"></div>');
+    }
+
+    if(min == 28 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-25"></div><div class="min-26"></div><div class="min-27"></div><div class="min-28"></div>');
+    }
+
+    if(min == 29 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-25"></div><div class="min-26"></div><div class="min-27"></div><div class="min-28"></div><div class="min-29"></div>');
+    }
+
+    if(min == 30 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div>');
+    }
+
+    if(min == 31 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-31"></div>');
+    }
+
+    if(min == 32 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-31"></div><div class="min-32"></div>');
+    }
+
+    if(min == 33 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-31"></div><div class="min-32"></div><div class="min-33"></div>');
+    }
+
+    if(min == 34 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-31"></div><div class="min-32"></div><div class="min-33"></div><div class="min-34"></div>');
+    }
+
+    if(min == 35 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-35"></div>');
+    }
+
+    if(min == 36 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-35"></div><div class="min-36"></div>');
+    }
+
+    if(min == 37 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-35"></div><div class="min-36"></div><div class="min-37"></div>');
+    }
+
+    if(min == 38 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-35"></div><div class="min-36"></div><div class="min-37"></div><div class="min-38"></div>');
+    }
+
+    if(min == 39 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-35"></div><div class="min-36"></div><div class="min-37"></div><div class="min-38"></div><div class="min-39"></div>');
+    }
+
+    if(min == 40 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div>');
+    }
+
+    if(min == 41 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-41"></div>');
+    }
+
+    if(min == 42 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-41"></div><div class="min-42"></div>');
+    }
+
+    if(min == 43 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-41"></div><div class="min-42"></div><div class="min-43"></div>');
+    }
+
+    if(min == 44 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-41"></div><div class="min-42"></div><div class="min-43"></div><div class="min-44"></div>');
+    }
+
+    if(min == 45 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-45"></div>');
+    }
+
+    if(min == 46 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-45"></div><div class="min-46"></div>');
+    }
+
+    if(min == 47 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-45"></div><div class="min-46"></div><div class="min-47"></div>');
+    }
+
+    if(min == 48 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-45"></div><div class="min-46"></div><div class="min-47"></div><div class="min-48"></div>');
+    }
+
+    if(min == 49 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-45"></div><div class="min-46"></div><div class="min-47"></div><div class="min-48"></div><div class="min-49"></div>');
+    }
+
+    if(min == 50 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div>');
+    }
+
+    if(min == 51 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-51"></div>');
+    }
+
+    if(min == 52 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-51"></div><div class="min-52"></div>');
+    }
+
+    if(min == 53 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-51"></div><div class="min-52"></div><div class="min-53"></div>');
+    }
+
+    if(min == 54 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-51"></div><div class="min-52"></div><div class="min-53"></div><div class="min-54"></div>');
+    }
+
+    if(min == 55 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-55"></div>');
+    }
+
+    if(min == 56 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-55"></div><div class="min-56"></div>');
+    }
+
+    if(min == 57 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-55"></div><div class="min-56"></div><div class="min-57"></div>');
+    }
+
+    if(min == 58 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-55"></div><div class="min-56"></div><div class="min-57"></div><div class="min-58"></div>');
+    }
+
+    if(min == 59 ) {
+        $('body').append('<div class="min-10"></div><div class="min-20"></div><div class="min-30"></div><div class="min-40"></div><div class="min-50"></div><div class="min-55"></div><div class="min-56"></div><div class="min-57"></div><div class="min-58"></div><div class="min-59"></div>');
+    }
+
+
