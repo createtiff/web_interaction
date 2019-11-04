168a9250f4769db47a0f8c1c56d67f1a049f9a4d
createtiff <createtiff@gmail.com>
2019-11-03 20:36:32 -0500

    Add files via upload

--- interface/scripts.js
+++ interface/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
 
-	var colorArray = ['red', '#00ff00', 'rgb(255,66,33)']
+	var colorArray = []
 	var randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
 	$('#box').css('background-color', randomColor)
 
-	var faces = ['A_A', 'B_B', 'C_C']
+	var faces = []
 	var randomFace = faces[Math.floor(Math.random()*faces.length)]
 	$('#box').text(randomFace)
 
